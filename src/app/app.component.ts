cdf0bb0938ef2ecb105356b19686c1b7f91932b2
asmaur <maur.a@outlook.com>
2024-05-08 05:51:07 -0300

    ngrx state management with angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from './actions/counter.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ngrx-tutorial';
+  counter$?: Observable<number>;
+  private store = inject(Store);
+
+  constructor(){
+    this.counter$ = this.store.select('counter');
+  }
+
+
+  increment(){
+    this.store.dispatch(increment());
+  }
+
+  decrement(){
+    this.store.dispatch(decrement())
+  }
+
+  reset(){
+    this.store.dispatch(reset());
+  }
+
 }
